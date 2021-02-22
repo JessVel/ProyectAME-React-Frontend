897f74a38b74d7dcce50521a0e25c97226daad5a
Jesica Velazquez <jesica.velazquez@ibm.com>
2021-02-22 12:19:05 -0300

    last changes in backend

--- backendMongoDB/src/server.js
+++ backendMongoDB/src/server.js
@@ -16,6 +16,8 @@ const PORT = process.env.PORT || 4000;
 
 app.use("/api/usuarios", require("./routes/user_routes"));
 app.use("/api/auth", require("./middleware/auth_routes"));
+app.use("/api/proyectos", require("./routes/proyecto_routes"));
+app.use("/api/tareas", require("./routes/tarea_routes"));
 
 app.listen(PORT, () => {
   success({
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import LogIn from "./components/auth/LogIn";
 import SingIn from "./components/auth/SingIn";
